dbdb9a3e8ffb9f1e1ffde93e578cd1320cb51fb8
rx570rohit <101522761+rx570rohit@users.noreply.github.com>
2022-04-20 00:32:31 +0530

    1

--- employee_wages/employee wages/emp_wage.cs
+++ employee_wages/employee wages/emp_wage.cs
@@ -11,28 +11,35 @@ namespace employee_wages
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+       
         public static void empcalculation()
-        {
+        {       int emphrs = 0, empwage = 0, totalEmpWages = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+                //variables
+               
+                Random obj = new Random();
+                int empcheck = obj.Next(1, 2);
 
-            int emphrs = 0;
-            int empwage = 0;
-            Random obj = new Random();
-            int empcheck = obj.Next(1, 2);
+                switch (empcheck)
+                {
+                    case IS_FULL_TIME:
+                        emphrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        emphrs = 4;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
 
-            switch (empcheck)
-            {
-                case IS_FULL_TIME:
-                    emphrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    emphrs = 4;
-                    break;
-                default:
-                    emphrs = 0;
-                    break;
+                empwage = emphrs * EMP_RATE_PER_HOUR;
+                totalEmpWages += empwage;
+                
             }
-            empwage = emphrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage :" + empwage);
+            Console.WriteLine("Total Emp Wage :" + totalEmpWages);
         }
     }
 }
