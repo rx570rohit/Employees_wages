2fd333b67a1390f18f8e27a0d74761a3495002fc
rx570rohit <101522761+rx570rohit@users.noreply.github.com>
2022-04-20 00:15:03 +0530

    master

---
+++ employee_wages/employee wages/emp_wage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_wages
+{
+    internal class emp_wage
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public static void empcalculation()
+        {
+
+            int emphrs = 0;
+            int empwage = 0;
+            Random obj = new Random();
+            int empcheck = obj.Next(1, 2);
+
+            switch (empcheck)
+            {
+                case IS_FULL_TIME:
+                    emphrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    emphrs = 4;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" + empwage);
+        }
+    }
+}
---
+++ employee_wages/employee wages/empmain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_wages
+{
+    internal class empmain
+    {
+        public static void Main(String[] arg)
+        {
+           emp_wage.empcalculation();
+        }
+    }
+}
